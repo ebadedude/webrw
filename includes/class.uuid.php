46e7c57d07ba8ff141dcea963257e9f78d520b76
Bade Iriabho <ebade@yahoo.com>
2011-10-14 08:46:13 -0500

    Changed variable from string to array but forgot to change strlen to count for the variable.

--- includes/class.uuid.php
+++ includes/class.uuid.php
@@ -74,7 +74,7 @@
 	    	$chars = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9');
 	    	$pass = '';
 	    	for ($i = 0; $i < $n-4; $i++){
-	    		$pass .= $chars[(rand() % strlen($chars))];
+	    		$pass .= $chars[(rand() % count($chars))];
 	    	}
 	    	
 	    	$tmp = microtime();
