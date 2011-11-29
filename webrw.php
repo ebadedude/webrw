1fb8d4423b27a0aa1e3cd9d6177805828b07a087
Bade Iriabho <ebade@yahoo.com>
2011-11-29 10:00:26 -0600

    Modified to handle new read functionality.

--- webrw.php
+++ webrw.php
@@ -1,15 +1,28 @@
-<?php 
+<?php
 	include_once 'includes/class.webrw.php';
 	
-	$action ='';
+	$action = '';
+	$mode = '';
+	$mimetype = '';
 	
 	if(isset($_GET['action'])) { $action = trim($_GET['action']); }
 	elseif(isset($_POST['action'])) { $action = trim($_POST['action']); }
 	
+	if(isset($_GET['mode'])) { $mode = trim($_GET['mode']); }
+	elseif(isset($_POST['mode'])) { $mode = trim($_POST['mode']); }
+	
+	if(isset($_GET['mimetype'])) { $mimetype = trim($_GET['mimetype']); }
+	elseif(isset($_POST['mimetype'])) { $mimetype = trim($_POST['mimetype']); }
+	
 	$my_webrw = new webrw();
 	switch($action) {
 		case 'read':
-			$my_webrw->read();
+			if(strcmp('exact',strtolower($mode)) == 0) {
+				if(strlen($mimetype) < 1) { $mimetype = DEFAULT_CONTENT_TYPE; }
+				$my_webrw->read($mimetype, READ_MODE_EXACT);
+			} else {
+				$my_webrw->read();
+			}
 			break;
 		case 'write':
 			$my_webrw->write();
