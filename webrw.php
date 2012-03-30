66ab7c30377c0e0087c56acc8816237a99271335
Bade Iriabho <ebade@yahoo.com>
2012-03-30 15:14:30 -0500

    Updated to reflect later additions

--- webrw.php
+++ webrw.php
@@ -1,15 +1,31 @@
 <?php
 	include_once 'includes/class.webrw.php';
 	
-	$action ='';
+	$action = '';
+	$mode = '';
+	$mimetype = '';
 	
-	if(isset($_GET['action'])) { $action = trim($_GET['action']); }
-	elseif(isset($_POST['action'])) { $action = trim($_POST['action']); }
+	if(isset($_GET['action'])) 			{ $action = trim($_GET['action']); }
+	elseif(isset($_POST['action'])) 	{ $action = trim($_POST['action']); }
+	
+	if(isset($_GET['mode'])) 			{ $mode = trim($_GET['mode']); }
+	elseif(isset($_POST['mode'])) 		{ $mode = trim($_POST['mode']); }
+	
+	if(isset($_GET['mimetype'])) 		{ $mimetype = trim($_GET['mimetype']); }
+	elseif(isset($_POST['mimetype'])) 	{ $mimetype = trim($_POST['mimetype']); }
 	
 	$my_webrw = new webrw();
+	
 	switch($action) {
 		case 'read':
-			$my_webrw->read();
+			if(strcmp('exact',strtolower($mode)) == 0) {
+				if(strlen($mimetype) < 1) {
+					$mimetype = DEFAULT_CONTENT_TYPE;
+				}
+				$my_webrw->read($mimetype, READ_MODE_EXACT);
+			} else {
+				$my_webrw->read();
+			}
 			break;
 		case 'write':
 			$my_webrw->write();
@@ -24,7 +40,13 @@
 			$my_webrw->kvupdate();
 			break;
 		default:
-			echo "Invalid action!!!";
+			if(isset($_GET['get']) || isset($_POST['get'])) {
+				$my_webrw->get();
+			} elseif(isset($_GET['set']) || isset($_POST['set'])) {
+				$my_webrw->set();
+			} else {
+				echo "Invalid action!!!";
+			}
 			break;
 	}
 ?>
