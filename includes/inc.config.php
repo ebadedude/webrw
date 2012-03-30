1a4d25b409ed3b79e607076d1bb15142f1c8b727
Bade Iriabho <ebade@yahoo.com>
2012-03-30 15:17:57 -0500

    Added more configuration choices

--- includes/inc.config.php
+++ includes/inc.config.php
@@ -8,11 +8,17 @@
 	 * Hold the configuration variables. Make changes here as needed.
 	 * 
 	 */
-	define('DEBUG_ON', TRUE);								//Turns on or off debugging
-	define('WORKSPACE_DIRECTORY', 'uploads');				//Directory name to use for server manipulated files, sits on root. Note: Apache needs to be able to write to this folder.
-	define('DS', DIRECTORY_SEPARATOR);						//Directory seperator
-	define('DEFAULT_CONTENT_TYPE', 'text/javascript');		//'text/javascript', 'text/html'
-	define('DEFAULT_CALLBACK', 'callback');					//default callback function name
-	define('DEFAULT_KEYVALUE_FILENAME', 'content');			//default name to use for key/value file names
+	define('READ_MODE_JSON', 'READ_MODE_JSON');		// when using webrw->read, reads and returns the specified source line by line 
+	define('READ_MODE_EXACT', 'READ_MODE_EXACT');		// when using webrw->read, reads and the specified source and return it in the specified mime type
+	
+	define('DEBUG_ON', FALSE);				// Turns on or off debugging
+	define('WORKSPACE_DIRECTORY', 'doc');			// Directory name to use for server manipulated files, sits on root
+	define('WORKSPACE_DELETE_AGE', 84600);			// How old files have to be before you delete them. Value is in seconds.
+	define('WORKSPACE_EXCLUDE', array('index.html'));	// File(s) to exclude from the deletion process 		
+	define('DS', DIRECTORY_SEPARATOR);			// Directory seperator
+	define('DEFAULT_CONTENT_TYPE', 'text/javascript');	// 'text/javascript', 'text/html'
+	define('DEFAULT_CALLBACK', 'callback');			// default callback function name
+	define('DEFAULT_KEYVALUE_FILENAME', 'content');		// default name to use for key/value file names
+	define('DEFAULT_READ_MODE', READ_MODE_JSON);		// select the default read mode
 	
 ?>
